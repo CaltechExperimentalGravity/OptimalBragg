3427f293f842c46ccd903fb1ea4aa6f2f0f79f53
controls <gautam.venugopalan@ligo.org>
2017-11-01 18:45:37 -0700

    updating cost functions

--- generic/runSwarm_aLIGO_ETM.m
+++ generic/runSwarm_aLIGO_ETM.m
@@ -42,7 +42,7 @@ NUMTOOLS.lambda = 1064e-9;            % Wavelength at which R and T are to be ev
 NUMTOOLS.opt_name       = 'aLIGO_ETM';
 NUMTOOLS.coatingType    = 'HR';
 NUMTOOLS.T_1            = 5e-6;      % Target transmission at 1064 nm --- 5ppm for HR
-NUMTOOLS.T_2p           = 0.968;      % Target transmission at 532 nm, p-pol ---- same as above
+NUMTOOLS.T_2p           = 0.02;      % Target transmission at 532 nm, p-pol ---- same as above
 NUMTOOLS.surfaceField   = 0.01;      % Target Surface E Field, [V/m]
 NUMTOOLS.aoi            = 0.;       % Angle of incidence (degrees)  --- 41.1deg for HR, 24.8deg for AR
 NUMTOOLS.aoi_green      = 0.;       % Angle of incidence (degrees)  ---24.746 deg for green
@@ -56,6 +56,7 @@ NUMTOOLS.include_TO   = 1;              %Include thermo-optic term in sensitivit
 
 ifo = SilicaTantala300;                 % Load a standard gwinc style parameter file with various material properties defined             
 ifo.Laser.Wavelength = NUMTOOLS.lambda;
+ifo.Materials.Coating.Phihighn = loss_highn;
 % load this lookup table for speedup
 load besselzeros.mat
 ifo.Constants.BesselZeros = besselzeros;
@@ -82,7 +83,7 @@ NUMTOOLS.ifo = ifo;
 % 11 = sensitivity to change in n2 @532nm p-pol +/-1%...
 % 12 = sensitivity of surface field term to perturbations in n1, n2 and L
 
-weights = [11111. 1111. 10. 0.0001 1e22 10. 10. 10. 10. 10. 10. 0.01];
+weights = [11111. 1111. 1. 10 1e22 50. 10. 50. 10. 50. 10. 0.01];
 NUMTOOLS.wt = weights;
 
 % setting the bounds for the variables to be searched over
@@ -91,8 +92,8 @@ UB = 0.51 * ones(size(x0));     % upper bound, lambda/2
 nvars = length(UB);
 
 if strcmp(NUMTOOLS.coatingType,'HR')
-%    LB(1) = 0.48;  %Constrain topost layer to be nearly halfwave thick...
-%    UB(1) = 0.52;
+%    LB(1) = 0.45;  %Constrain topost layer to be nearly halfwave thick...
+%    UB(1) = 0.55;
 	NUMTOOLS.firstLayer = 'SiO2';     %Depending whether coating is on the HR side or AR side
 else
 	NUMTOOLS.firstLayer = 'Ta2O5';
@@ -103,12 +104,12 @@ hybridopts = optimoptions('fmincon',...
                   'Display','iter',...
                   'MaxIter', 911,...
                   'TolFun', 1e-4,...
-                  'MaxFunEvals', 11111);
+                  'MaxFunEvals', 51111);
 
 options = optimoptions('particleswarm',...
                'SwarmSize', nvars*155,...     % 
                'UseParallel', 1,...
-               'MaxIter', 911,...
+               'MaxIter', 1111,...
                'SelfAdjustment',   1.49,...
                'SocialAdjustment', 1.49,...
                'TolFun', 1e-4,...
