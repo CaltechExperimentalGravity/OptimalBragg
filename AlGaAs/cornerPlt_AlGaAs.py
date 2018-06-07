588bf0f5f4e596f0d8dc004b2035eaeb4e7d9551
rxa254 <rana@caltech.edu>
2018-06-07 02:11:51 -0700

    1) use argparse for arg parsing 2) load newest .h5 file by default for plotting

--- AlGaAs/cornerPlt_AlGaAs.py
+++ AlGaAs/cornerPlt_AlGaAs.py
@@ -10,18 +10,31 @@ The above will make a corner plot from the data in aLIGO_ETM_MC.hdf5
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-import sys
+#import sys
 import matplotlib
 import corner
 from scipy.io import loadmat
+import os
+import glob
+
+import argparse
+
+newest = max(glob.iglob('MCout/*.[Hh]5'), key=os.path.getctime)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filename", type=str, default=newest,
+                    help="file with MCMC data")
+args = parser.parse_args()
 
 # use argparse instead
-hdfFileName = sys.argv[1]
+hdfFileName = args.filename
 
 #if 'gvELOG' in plt.style.available:
 #    plt.style.use('gvELOG')
 #else:
 #    plt.style.use('bmh')
+
+
 
 #Open the file, load the data
 f = h5py.File(hdfFileName,'r')
@@ -32,7 +45,7 @@ samples[3,-1] = samples[3,-2]
 
 # TODO: add text about deposition errors on the plot in the unused space
 # Make the plot
-fig,ax = plt.subplots(np.shape(samples)[1], np.shape(samples)[1], figsize=(18,18))
+fig,ax = plt.subplots(np.shape(samples)[1], np.shape(samples)[1], figsize=(12,12))
 #fig.subplots_adjust(wspace=0.5,hspace=0.35)
 corner.corner(samples,
         labels=['$\\mathrm{T}_{1064 \\mathrm{nm}}$ [ppm]', 
@@ -42,11 +55,13 @@ corner.corner(samples,
             #quantiles=[0.9, 0.95, 0.98],
             show_titles=True, use_math_text=True,
             bins=50,
-            range=[(0,20), (0.4,1.4), (2.27,2.34), (0,15)],
+            range=[(0,20), (0.4,1.4), (2.24,2.34), (0,15)],
             #   levels=(0.95,),
-	    color='xkcd:umber',
-            hist_kwargs={'linewidth':2},
-            label_kwargs={'fontsize':16, 'fontweight':'bold'},
-            title_kwargs={'fontsize':16, 'fontweight':'bold'}, fig=fig)
+	    color='xkcd:poop',
+            hist_kwargs={'linewidth':2.5},
+            label_kwargs={'fontsize':'large', 'fontweight':'bold'},
+            title_kwargs={'fontsize':'large', 'fontweight':'bold'}, fig=fig)
 
-plt.savefig(hdfFileName + '.pdf')
+fubu = hdfFileName + '.pdf'
+print("File saved as " + fubu)
+plt.savefig(fubu)
