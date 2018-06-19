310c1ec1aa414ecf48f83cd696315517ff30491b
rxa254 <rana@caltech.edu>
2018-06-18 21:26:01 -0700

    different fonts

--- AlGaAs/cornerPlt_AlGaAs.py
+++ AlGaAs/cornerPlt_AlGaAs.py
@@ -3,7 +3,8 @@ Script to take output of MC analysis and make a corner plot
 Example usage:
     python cornerPlt.py aLIGO_ETM_MC.hdf5
 
-The above will make a corner plot from the data in aLIGO_ETM_MC.hdf5
+Plot latest file in MCout/ dir:
+    python cornerPlt.py
 
 '''
 
@@ -11,11 +12,31 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 #import sys
-import matplotlib
+import matplotlib as mpl
 import corner
 from scipy.io import loadmat
 import os
 import glob
+
+mpl.rcParams.update({'text.usetex': False,
+                     'lines.linewidth': 2.5,
+                     'font.size': 10,
+                     'xtick.labelsize': 'small',
+                     'ytick.labelsize': 'small',
+                     'axes.labelsize': 'x-small',
+                     'axes.titlesize': 'x-small',
+                     'axes.grid': True,
+                     'grid.alpha': 0.73,
+                     'lines.markersize': 12,
+                     'legend.borderpad': 0.2,
+                     'legend.fancybox': True,
+                     'legend.fontsize': 13,
+                     'legend.framealpha': 0.7,
+                     'legend.handletextpad': 0.1,
+                     'legend.labelspacing': 0.2,
+                     'legend.loc': 'best',
+                     'savefig.dpi': 80,
+                     'pdf.compression': 9})
 
 import argparse
 
@@ -38,22 +59,25 @@ samples[3,-1] = samples[3,-2]
 
 # TODO: add text about deposition errors on the plot in the unused space
 # Make the plot
-fig,ax = plt.subplots(np.shape(samples)[1], np.shape(samples)[1], figsize=(12,12))
+fig,ax = plt.subplots(np.shape(samples)[1],
+                      np.shape(samples)[1], figsize=(12,12))
 #fig.subplots_adjust(wspace=0.5,hspace=0.35)
 corner.corner(samples,
         labels=['$\\mathrm{T}_{1064} \\mathrm{[ppm]}$', 
             '$\\mathrm{S}_{\\mathrm{TO}} [\\times 10^{-21} \\mathrm{m}/\\sqrt{\\mathrm{Hz}}]$',
             '$\\mathrm{S}_{\\mathrm{Br}} [\\times 10^{-21} \\mathrm{m}/\\sqrt{\\mathrm{Hz}}]$',
             '$\\vec{E}_{\\mathrm{Surf}} \\mathrm{[V/m]}$',
-	    'Absorption [ppm]'],
+            '$\\mathrm{Absorption [ppm]}$'],
             #quantiles=[0.9, 0.95, 0.98],
-            truths = [5.8, 0.43, 2.25, 1.9, 0.8],
-            show_titles=True, use_math_text=True,
+            truths = [5, 1, 2, 1, 0.5],
+            truth_color = 'xkcd:duck egg blue',
+            show_titles = True,
+            use_math_text = True,
             bins = 50,
-            range=[(0,20), (0,1.4), (2.2,2.4), (0,15), (0.7,1.1)],
+            #range=[(0,20), (0, 8), (2.0, 2.5), (0, 20), (0, 3)],
             #   levels=(0.95,),
-            color = 'xkcd:browny orange',
-            smooth = 1,
+            color = 'xkcd:cerulean blue',
+            smooth = 0.5, # smoothing scale in std's
             hist_kwargs  = {'linewidth':2.5},
             label_kwargs = {'fontsize':'large', 'fontweight':'bold'},
             title_kwargs = {'fontsize':'medium', 'fontweight':'bold'},
