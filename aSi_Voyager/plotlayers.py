dd4fe9702e4a600f2f57bd93923ab2930495a822
Rana <rana@caltech.edu>
2021-01-22 01:02:04 -0800

    added substrate noise traces

--- aSi_Voyager/plotlayers.py
+++ aSi_Voyager/plotlayers.py
@@ -39,8 +39,8 @@ plt.rcParams.update({'text.usetex': False,
                      'axes.grid.axis': 'both',
                      'axes.grid.which': 'both',
                      'axes.grid': True,
-                     'grid.color': 'xkcd:beige',
-                     'grid.alpha': 0.53,
+                     'grid.color': 'xkcd:Cerulean',
+                     'grid.alpha': 0.2,
                      'lines.markersize': 12,
                      'legend.borderpad': 0.2,
                      'legend.fancybox': True,
@@ -59,10 +59,9 @@ plt.rcParams.update({'text.usetex': False,
 # load Data file from run of mkMirror.py
 fname = max(glob.iglob('Data/*Layers*.mat'), key=os.path.getctime)
 fname = fname[5:] # rm 'data' from the name
-fname = 'ETM_Layers_190517_1828.mat'
+#fname = 'ETM_Layers_190519_1459.mat'
 if __debug__:
     print('Loading ' + fname + '...')
-
 
 z       = scio.loadmat('Data/' + fname, squeeze_me=True)
 n       = z['n']
@@ -70,7 +69,7 @@ T       = z['T']
 
 
 if __debug__:
-    print('Loading ' + 'gwinc.ifo' + '...')
+    print('Loading ' + 'gwinc.ifo' + ' ' + fname)
     tic = default_timer()
 
 ifo     = gwinc.Struct.from_file(z["ifo_name"])
@@ -86,15 +85,17 @@ rr, _   = multidiel1(n, z['L'], lams)
 RR      = np.abs(rr)**2
 TT      = 1 - RR
 
-#convert from optical thickness to physical thickness
+#  convert from optical thickness to physical thickness
 L       = lambda0 * op2phys(z['L'], n[1:-1])
-# calculate field v. distance into coating
+#  calculate field v. distance into coating
 Z,field = fieldDepth(L, n, pol='p', nPts=300)
 layers  = np.cumsum(1e6 * L)
 layers  = np.append(0, layers)
 
 if __debug__:
     print('Make plots...')
+    tic = default_timer()
+
 fig, ax = plt.subplots(1,1)
 ax.semilogy(1e6*lams*lambda0, TT,
                 lw=3, label='Transmissivity', c='xkcd:Red')
@@ -113,7 +114,7 @@ if __debug__:
     print('Transmission of this coating at {} nm is {} ppm'.format(
         1e9*lambda0, round(1e6*T,2)))
 
-plt.savefig('Figures/' + 'ETM_R' + '.pdf', bbox_inches='tight')
+plt.savefig('Figures/' + 'ETM_R' + '.pdf')
 
 
 # Make the plotof the Layer structure
@@ -146,11 +147,11 @@ fig.subplots_adjust(hspace=0.01,left=0.09,right=0.95,top=0.92)
 plt.suptitle('a-Si:SiO$_2$ coating electric field')
 
 
-plt.savefig('Figures/' + fname[:-4] + '.pdf', bbox_inches='tight')
-plt.savefig('Figures/' + 'ETM_Layers' + '.pdf', bbox_inches='tight')
+plt.savefig('Figures/' + fname[:-4] + '.pdf')
+plt.savefig('Figures/' + 'ETM_Layers' + '.pdf')
 
 
-# plot the Thermal Noise
+# ----  plot the Thermal Noise
 ff = np.logspace(0, 4, 500)
 fig3, ax3 = plt.subplots(1,1)
 # Build up a "mirror" structure as required by pygwinc
@@ -160,17 +161,26 @@ StoZ, SteZ, StrZ, _ = gwinc.noise.coatingthermal.coating_thermooptic(ff,
                                                 mir, ifo.Laser.Wavelength, ifo.Optics.ETM.BeamRadius)
 SbrZ = gwinc.noise.coatingthermal.coating_brownian(ff, mir, ifo.Laser.Wavelength, ifo.Optics.ETM.BeamRadius)
 
-Larm = 4000
+subBrown = gwinc.noise.substratethermal.substrate_brownian(ff, mir, ifo.Optics.ETM.BeamRadius)
+subTE    = gwinc.noise.substratethermal.substrate_thermoelastic(ff, mir, ifo.Optics.ETM.BeamRadius)
 
-ax3.loglog(ff, Larm * np.sqrt(StoZ), label='Thermo-Optic')
-ax3.loglog(ff, Larm * np.sqrt(SteZ), label='Thermo-Elastic')
-ax3.loglog(ff, Larm * np.sqrt(StrZ), label='Thermo-Refractive')
-ax3.loglog(ff, Larm * np.sqrt(SbrZ), label='Brownian')
+Larm = 1 #4000
+
+ax3.loglog(ff, Larm * np.sqrt(StoZ), label='Thermo-Optic', c='xkcd:Purplish Blue')
+ax3.loglog(ff, Larm * np.sqrt(SteZ), label='Thermo-Elastic', c='xkcd:Golden')
+ax3.loglog(ff, Larm * np.sqrt(StrZ), label='Thermo-Refractive', c='xkcd:Puke')
+ax3.loglog(ff, Larm * np.sqrt(SbrZ), label='Brownian', c='xkcd:Tomato')
+ax3.loglog(ff, Larm * np.sqrt(subBrown), label='Substrate Brownian', c='xkcd:Dusty Blue')
+ax3.loglog(ff, Larm * np.sqrt(subTE), label='Substrate Thermo-Elastic', c='xkcd:Chocolate', alpha=0.3)
 ax3.legend()
-#ax3.set_ylim([8e-22, 2e-20])
+ax3.set_ylim([8e-24, 2e-20])
 
-ax3.grid(which='major', alpha=0.6)
-ax3.grid(which='minor', alpha=0.4)
+#ax3.grid(which='major', alpha=0.6)
+#ax3.grid(which='minor', alpha=0.4)
 ax3.set_ylabel('Displacement Noise $[\\mathrm{m} / \\sqrt{\\mathrm{Hz}}]$')
 ax3.set_xlabel('Frequency [Hz]')
 plt.savefig('Figures/' + 'ETM_TN.pdf')
+
+if __debug__:
+    dt = default_timer() - tic
+    print('Took ' + str(round(dt,3)) + ' sec to make the plots and save them.')
