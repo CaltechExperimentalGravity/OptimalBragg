7bfdd8d5801d25e5af5bd12f8ac195f2997ca9a8
pacosalces <pacosalces@gmail.com>
2023-07-20 14:35:40 -0700

    add setup.py so we can install as package

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coatings",
+    version="1.0.0",
+    author="pacosalces",
+    author_email="pacosalces@gmail.com",
+    description="Package for the design and optimization of dielectric coatings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://git.ligo.org/40m/Coatings/coatings",
+    license="LICENSE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "pandas",
+        "h5py",
+        "pyyaml",
+        "physunits",
+        "gwinc",
+        "corner",
+        "emcee",
+    ],
+    python_requires=">=3.9",
+)
