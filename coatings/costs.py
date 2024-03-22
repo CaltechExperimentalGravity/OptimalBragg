fix bug on HR spectral bandwidth by removing superfluous center wavelength scaling in layers.py, and cleaned up the other methods or calls that reference it

coatings/costs.py
@@ -22,7 +22,7 @@ def norm(norm_arg):
 
 
 @norm("l2")
-def trans_cost(Ls, target, stack, lamb, **multilayer_diel_pars):
+def trans_cost(Ls, target, stack, **multilayer_diel_pars):
     """Evaluates a cost based on stack reflectivity/transmission
 
     Args:
@@ -41,16 +41,14 @@ def trans_cost(Ls, target, stack, lamb, **multilayer_diel_pars):
     Deleted Parameters:
 
     """
-    ns = stack["ns"]
-    rr, _ = multilayer_diel(
-        ns, Ls, lamb, stack["lam_ref"], **multilayer_diel_pars
-    )
+    ns, lamb = stack["ns"], stack["lam_ref"]
+    rr, _ = multilayer_diel(ns, Ls, lamb, **multilayer_diel_pars)
     T = 1 - np.abs(rr) ** 2
     return np.abs((target - T) / target)
 
 
 @norm("l1")
-def sens_cost(Ls, target, stack, lamb, **multilayer_diel_pars):
+def sens_cost(Ls, target, stack, **multilayer_diel_pars):
     """Evaluates the sensitivity cost of a stack transmission
     at given wavelength relative to a 1% Ls perturbation
 
@@ -59,26 +57,21 @@ def sens_cost(Ls, target, stack, lamb, **multilayer_diel_pars):
         ns (arr): Refractive index
         Ls (arr): Physical thickness
         lamb (float): Wavelength at which to evaluate sens
-        lamb_0 (float): Reference wavelength
         multilayer_diel_pars (dict, optional): Other parameters for evaluating
                                                the transmission.
     Returns:
         (float): Scalar cost for the evaluated sens
     """
-    ns = stack["ns"]
-    rr0, _ = multilayer_diel(
-        ns, Ls, lamb, stack["lam_ref"], **multilayer_diel_pars
-    )
+    ns, lamb = stack["ns"], stack["lam_ref"]
+    rr0, _ = multilayer_diel(ns, Ls, lamb, **multilayer_diel_pars)
     T0 = 1 - np.abs(rr0) ** 2
-    rr1, _ = multilayer_diel(
-        ns, 1.01 * Ls, lamb, stack["lam_ref"], **multilayer_diel_pars
-    )
+    rr1, _ = multilayer_diel(ns, 1.01 * Ls, lamb, **multilayer_diel_pars)
     T1 = 1 - np.abs(rr1) ** 2
     return np.abs(100 * (T0 - T1) / T0)
 
 
 @norm("arcsinh")
-def surfield_cost(Ls, target, stack, lamb, **multilayer_diel_pars):
+def surfield_cost(Ls, target, stack, **multilayer_diel_pars):
     """Evaluates the normalized surface E field cost
 
     Args:
@@ -92,10 +85,8 @@ def surfield_cost(Ls, target, stack, lamb, **multilayer_diel_pars):
        Returns:
            cost (float): Scalar cost for the normalized surf E-field
     """
-    ns = stack["ns"]
-    rr, _ = multilayer_diel(
-        ns, Ls, lamb, stack["lam_ref"], **multilayer_diel_pars
-    )
+    ns, lamb = stack["ns"], stack["lam_ref"]
+    rr, _ = multilayer_diel(ns, Ls, lamb, **multilayer_diel_pars)
     return np.abs(1 + rr) ** 2
 
 

coatings/layers.py
@@ -4,7 +4,7 @@ import scipy.io as scio
 from scipy.interpolate import interp1d, PchipInterpolator
 
 
-def multilayer_diel(ns, Ls, lamb, lamb_0, aoi=0, pol="te"):
+def multilayer_diel(ns, Ls, lamb, aoi=0, pol="te"):
     """Calculates amplitude reflectivity and complex
     impedance of a dielectric stack according to Eqs. (6.1.3)
     and (7.7.1) of http://eceweb1.rutgers.edu/~orfanidi/ewa/
@@ -17,10 +17,8 @@ def multilayer_diel(ns, Ls, lamb, lamb_0, aoi=0, pol="te"):
                  the dielectric stack, ordered from incident
                  to transmitted medium. Should have 2 fewer
                  elements than n.
-        lamb (float, arr): Wavelengths for which to evaluate
+        lamb (float, arr): Wavelength(s) for which to evaluate
                            the stack reflectivity.
-        lamb_0 (float): Center design wavelength for which to
-                        evaluate the stack reflectivity.
         aoi (float, optional): Angle of incidence in rad,
                                default=0.0 (normal inc)
         pol (str, optional): Polarization at which to evaluate
@@ -33,8 +31,8 @@ def multilayer_diel(ns, Ls, lamb, lamb_0, aoi=0, pol="te"):
                    incident medium with n=1.0, i.e. vacuum.
 
     """
-    # Use center design wavelength as reference unit
-    rel_lamb = lamb / lamb_0
+    # Wavenumbers
+    ki = 2 * np.pi / lamb
 
     # Cosine of theta; projection for oblique incidence
     proj_aoi = np.conj(1 - (ns[0] * np.sin(aoi) / ns) ** 2)
@@ -49,19 +47,19 @@ def multilayer_diel(ns, Ls, lamb, lamb_0, aoi=0, pol="te"):
         nT = ns / costh
 
     # Optical thicknesses
-    opt_L = (ns[1:-1] * Ls / lamb) * costh[1 : len(Ls) + 1]
+    opt_L = (ns[1:-1] * Ls) * costh[1 : len(Ls) + 1]
 
     # Per-layer amplitude reflectivity
     r = -np.diff(nT) / (np.diff(nT) + 2 * nT[0 : len(Ls) + 1])
 
     # Initialize to incident layer amplitude reflectivity
-    Gamma_0 = r[len(Ls)] * np.ones_like(rel_lamb)
+    Gamma_0 = r[len(Ls)] * np.ones_like(lamb)
 
     # Recursion relation
     for i in range(len(Ls) - 1, -1, -1):
-        delta = 2 * np.pi * opt_L[i] / rel_lamb
-        z = np.exp(-2 * 1j * delta)
-        Gamma_0 = (r[i] + Gamma_0 * z) / (1 + r[i] * Gamma_0 * z)
+        delta_i = ki * opt_L[i]
+        z_i = np.exp(2 * 1j * delta_i)
+        Gamma_0 = (r[i] + Gamma_0 * z_i) / (1 + r[i] * Gamma_0 * z_i)
 
     # Incident layer impedance
     Z_0 = (1 + Gamma_0) / (1 - Gamma_0)
@@ -94,8 +92,8 @@ def field_zmag(ns, Ls, lam, aoi=0, pol="s", n_pts=30):
 
     Args:
         ns (arr): Refractive index
-        Ls (arr): Physical thickness
-        lam (float): Wavelength
+        Ls (arr): Physical thickness [m]
+        lam (float): Reference wavelength [m]
         aoi (float, optional): Angle of incidence (rad),
                                default = 0
         pol (str, optional): Polarization, default = 's'.
@@ -216,32 +214,43 @@ def calc_abs(Esq, Ls, alphas):
 
 
 def stack_refl(wavelengths, stack):
-    lam_ref = stack["lam_ref"]
     ns, Ls = stack["ns"], stack["Ls"]
     try:
         iter(wavelengths)
         refl = np.zeros_like(wavelengths)
         for ii, wavelength in enumerate(wavelengths):
-            rr, _ = multilayer_diel(ns, Ls, wavelength, lam_ref)
+            rr, _ = multilayer_diel(ns, Ls, wavelength)
             refl[ii] = rr
     except TypeError:
-        rr, _ = multilayer_diel(ns, Ls, wavelengths, lam_ref)
+        rr, _ = multilayer_diel(ns, Ls, wavelengths)
         refl = rr
     finally:
         return refl
 
 
 def stack_R(wavelengths, stack):
-    lam_ref = stack["lam_ref"]
     ns, Ls = stack["ns"], stack["Ls"]
     try:
         iter(wavelengths)
         RR = np.zeros_like(wavelengths)
         for ii, wavelength in enumerate(wavelengths):
-            rr, _ = multilayer_diel(ns, Ls, wavelength, lam_ref)
+            rr, _ = multilayer_diel(ns, Ls, wavelength)
             RR[ii] = np.abs(rr) ** 2
     except TypeError:
-        rr, _ = multilayer_diel(ns, Ls, wavelengths, lam_ref)
+        rr, _ = multilayer_diel(ns, Ls, wavelengths)
         RR = np.abs(rr) ** 2
     finally:
         return RR
+
+
+def qwbandedges(stack):
+    """Based on Eq (6.3.18) in orfanidi"""
+    ns = stack["ns"][1:-1]
+    nH = max(ns)
+    nL = min(ns)
+    LH = stack["Ls"][np.argmax(ns)]
+    LL = stack["Ls"][np.argmin(ns)]
+    rho = (nH - nL) / (nH + nL)
+    lam1 = np.pi * (nH * LH + nL * LL) / (np.arccos(-rho))
+    lam2 = np.pi * (nH * LH + nL * LL) / (np.arccos(rho))
+    return lam1, lam2

coatings/optimizer.py
@@ -80,7 +80,6 @@ def vector_cost(Ls, stack, costs, Sto_pars=None, Sbr_pars=None, verbose=False):
     Returns:
         vector_cost (dict): Weighted evaluated costs array
     """
-    lam_ref = stack["lam_ref"]
     ns = stack["ns"]
     vector_cost = {}
     vector_weights = {}
@@ -91,7 +90,7 @@ def vector_cost(Ls, stack, costs, Sto_pars=None, Sbr_pars=None, verbose=False):
                     if specs["weight"][lam]:
                         vector_cost[cost + f"{int(lam/nm):d}"] = specs[
                             "weight"
-                        ][lam] * trans_cost(Ls, target, stack, lam)
+                        ][lam] * trans_cost(Ls, target, stack)
                         vector_weights[cost + f"{int(lam/nm):d}"] = specs[
                             "weight"
                         ][lam]
@@ -100,13 +99,13 @@ def vector_cost(Ls, stack, costs, Sto_pars=None, Sbr_pars=None, verbose=False):
                     if specs["weight"][lam]:
                         vector_cost[cost + f"{int(lam/nm):d}"] = specs[
                             "weight"
-                        ][lam] * surfield_cost(Ls, target, stack, lam_ref)
+                        ][lam] * surfield_cost(Ls, target, stack)
                         vector_weights[cost + f"{int(lam/nm):d}"] = specs[
                             "weight"
                         ][lam]
             if cost == "Lsens":
                 vector_cost[cost] = specs["weight"] * sens_cost(
-                    Ls, specs["target"], stack, lam_ref
+                    Ls, specs["target"], stack
                 )
                 vector_weights[cost] = specs["weight"]
             if cost == "Sbr":
@@ -169,15 +168,14 @@ def vector_score(Ls, stack, costs, to_pars, br_pars):
     Returns:
         (float): Normalized overlap between the vectors
     """
-    lam_ref = stack["lam_ref"]
-    ns = stack["ns"]
+    ns, lam_ref = stack["ns"], stack["lam_ref"]
     rel_score = {}
     for cost, specs in costs.items():
         if specs["weight"]:
             if cost == "T":
                 for lam, target in specs["target"].items():
                     if specs["weight"][lam]:
-                        rr, _ = multilayer_diel(ns, Ls, lam, lam_ref)
+                        rr, _ = multilayer_diel(ns, Ls, lam)
                         actual = 1 - np.abs(rr) ** 2
                         abserr = np.abs(
                             (min(actual, target) - max(actual, target))
@@ -186,23 +184,21 @@ def vector_score(Ls, stack, costs, to_pars, br_pars):
             if cost == "Esurf":
                 for lam, target in specs["target"].items():
                     if specs["weight"][lam]:
-                        rr, _ = multilayer_diel(ns, Ls, lam, lam_ref)
+                        rr, _ = multilayer_diel(ns, Ls, lam)
                         actual = surfield(rr, normalized=True)
                         abserr = np.abs(
                             (min(actual, target) - max(actual, target))
                         )
                         rel_score[cost] = np.abs(max(actual, target)) / abserr
-                        # rel_score[cost] = 1 - np.abs((target - actual) / target)
             if cost == "Lsens":
-                rrplus, _ = multilayer_diel(ns, Ls + 1e-9, lam_ref, lam_ref)
-                rr0, _ = multilayer_diel(ns, Ls, lam_ref, lam_ref)
+                rrplus, _ = multilayer_diel(ns, 1.01 * Ls, lam_ref)
+                rr0, _ = multilayer_diel(ns, Ls, lam_ref)
                 Tplus = 1 - np.abs(rrplus) ** 2
                 T0 = 1 - np.abs(rr0) ** 2
-                actual = np.abs(Tplus - T0) / 1e-9
+                actual = 100 * np.abs(Tplus - T0)
                 target = specs["target"]
                 abserr = np.abs((min(actual, target) - max(actual, target)))
                 rel_score[cost] = np.abs(max(actual, target)) / abserr
-                # rel_score[cost] = 1 - np.abs((target - actual) / target)
             if cost == "Sbr":
                 actual = coating_brownian(
                     np.array([100 * Hz]),
@@ -214,26 +210,22 @@ def vector_score(Ls, stack, costs, to_pars, br_pars):
                 target = specs["target"]
                 abserr = np.abs((min(actual, target) - max(actual, target)))
                 rel_score[cost] = np.abs(max(actual, target)) / abserr
-                # rel_score[cost] = 1 - np.abs((target - actual) / target)
             if cost == "Sto":
                 actual, _, _ = coating_thermooptic(100 * Hz, stack, **to_pars)
                 target = specs["target"]
                 abserr = np.abs((min(actual, target) - max(actual, target)))
                 rel_score[cost] = np.abs(max(actual, target)) / abserr
-                # rel_score[cost] = 1 - np.abs((target - actual) / target)
             if cost == "abs":
                 _, Enorm = field_zmag(
-                    stack["ns"], Ls, n_pts=2**6, lam=stack["lam_ref"]
+                    stack["ns"], Ls, n_pts=2**6, lam=lam_ref
                 )
                 actual = calc_abs(Enorm, Ls, stack["alphas"])
                 target = specs["target"]
                 abserr = np.abs((min(actual, target) - max(actual, target)))
                 rel_score[cost] = np.abs(max(actual, target)) / abserr
-                # rel_score[cost] = 1 - np.abs((target - actual) / target)
             if cost == "Lstdev":
                 actual = np.std(Ls) / np.mean(Ls)
                 target = specs["target"]
                 abserr = np.abs((min(actual, target) - max(actual, target)))
                 rel_score[cost] = np.abs(max(actual, target)) / abserr
-                # rel_score[cost] = 1 - np.abs((target - actual) / target)
     return rel_score

coatings/plot.py
@@ -105,21 +105,24 @@ def plot_spectral(wavelengths, stack, markers=[]):
         stack (dict): Stack dictionary containing at least n,L keys
         markers (list, optional): Wavelengths of interest
     """
-    lam_ref = stack["lam_ref"]
     ns, Ls = stack["ns"], stack["Ls"]
-
     design_Rs = np.zeros(len(markers) + 1)
-    markers.append(lam_ref)
+    markers.append(stack["lam_ref"])
     for ii, lam in enumerate(markers):
-        rrs, _ = multilayer_diel(ns, Ls, lam, lam_ref)
+        rrs, _ = multilayer_diel(ns, Ls, lam)
         design_Rs[ii] = np.abs(rrs) ** 2
     design_Ts = 1 - design_Rs
 
     # Spectral reflection and transmission
     RR = stack_R(wavelengths, stack)
     TT = 1 - RR
+    lam1, lam2 = qwbandedges(stack)
 
     fig, ax = plt.subplots(1, 1)
+    # HR bandwidth wavelengths, assuming qw stack:
+    # ax.axvline((lam1 / um), label="lam1", ls="--")
+    # ax.axvline((lam2 / um), label="lam2", ls="--")
+
     ax.semilogy(
         wavelengths / um,
         TT,