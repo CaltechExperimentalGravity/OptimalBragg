af5b352ad7a1093d2736ad5837632c0c418dc525
pacosalces <45318932+pacosalces@users.noreply.github.com>
2024-04-13 11:38:02 -0700

    Update setup.py after renaming

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="coatings",
+    name="OptimalBragg",
     version="1.0.0",
     author="pacosalces",
     author_email="pacosalces@gmail.com",
     description="Package for the design and optimization of dielectric coatings",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git.ligo.org/francisco.carcoba/Coatings/",
+    url="https://github.com/CaltechExperimentalGravity/OptimalBragg/",
     license="LICENSE",
     packages=setuptools.find_packages(),
     classifiers=[
